e81e5a093b2fbbcda4293f615a0ce500b0248d5a
savanna chiang <savannaac@gmail.com>
2021-09-09 00:01:39 -0500

    updates gemfile

--- rails-backend/app/models/user.rb
+++ rails-backend/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :messages
+    has_many :participations
+    has_many :conversations, through :participations
+
+    validates_presence_of :email, uniqueness: true 
 end
--- rails-backend/app/models/user_convo.rb
+++
@@ -1,2 +0,0 @@
-class UserConvo < ApplicationRecord
-end
