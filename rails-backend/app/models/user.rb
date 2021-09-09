0a3c0177d5083e3cb948069c643844494125308d
savanna chiang <savannaac@gmail.com>
2021-09-09 00:28:37 -0500

    adjusts for devise

--- rails-backend/app/models/user.rb
+++ rails-backend/app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-    has_many :messages
-    has_many :participations
-    has_many :conversations, through :participations
-
-    validates_presence_of :email, uniqueness: true 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- rails-backend/db/schema.rb
+++ rails-backend/db/schema.rb
@@ -33,12 +33,4 @@ ActiveRecord::Schema.define(version: 2021_09_09_043416) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
