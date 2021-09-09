a4d02399673e372c71aafbc4aca6841e6d99bb96
savanna chiang <savannaac@gmail.com>
2021-09-09 01:08:26 -0500

    adjusts for jwt

--- rails-backend/db/schema.rb
+++ rails-backend/db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_053451) do
+ActiveRecord::Schema.define(version: 2021_09_09_055724) do
 
   create_table "conversations", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
   create_table "messages", force: :cascade do |t|
