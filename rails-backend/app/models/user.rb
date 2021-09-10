03f93df050e6b6ed30a78359afa870449bd28631
savanna chiang <savannaac@gmail.com>
2021-09-09 23:01:55 -0500

    adjusts for devise-jwt

--- rails-backend/app/models/user.rb
+++ rails-backend/app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenyList
+  
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenyList
+
+  devise :database_authenticatable,
+  :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   has_many :messages
   has_many :participations
--- rails-backend/config/routes.rb
+++ rails-backend/config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
       devise_for :users, controllers: {
         :registrations => "api/#{version}/users/registrations",
         :sessions => "api/#{version}/users/sessions",
-        :passwords => "api/#{version}/users/passwords"
+        # :passwords => "api/#{version}/users/passwords"
       }
       
       resources :messages
       resources :conversations
       resources :participations
+
+      get "/user-data", to: "users#show"
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
