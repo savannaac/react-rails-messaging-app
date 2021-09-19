74f59a2cd26c59cf9848da2fbaf523af52dabac4
savanna chiang <savannaac@gmail.com>
2021-09-19 00:35:38 -0500

    adds styling to profile component

--- react-frontend/src/components/User/Profile.js
+++ react-frontend/src/components/User/Profile.js
@@ -3,7 +3,9 @@ import React from "react";
 export default class Profile extends React.Component {
     state = {
         username: "Sav",
-        createdAt: "today"
+        avatar: "https://i.ibb.co/kXC4Z2R/65136031766-8-BA4-B1-A2-1-C09-492-D-945-B-736-F9-AC31200.jpg",
+        createdAt: "today",
+        updatedAt: "also today"
     };
 
      // handleClick = (e) => {
@@ -20,9 +22,12 @@ export default class Profile extends React.Component {
                     <button className="exit-button" onClick={this.handleClick}>x</button>
                 </div>
 
+                <img className="avatar" src={this.state.avatar} alt="user-avatar" />
+
                 <div className="profile">
-                    <div className="profile-username">{this.state.username}</div>
-                    <div className="profile-createdAt">member since: {this.state.createdAt}</div>
+                    <p className="profile-username">{this.state.username}</p>
+                    <p className="profile-createdAt">member since: {this.state.createdAt}</p>
+                    <p className="profile-updatedAt">last active: {this.state.updatedAt}</p>
                 </div>
               
             </div>
