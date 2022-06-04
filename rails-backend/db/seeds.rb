a8d4a398f283f3c5ec86502f2f6daf7dbacd7d0f
savanna chiang <savannaac@gmail.com>
2022-06-04 00:32:48 -0500

    nope, went back to sqlite 3

--- rails-backend/db/seeds.rb
+++ rails-backend/db/seeds.rb
@@ -6,11 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(username: "Sav", email: "savannaac@gmail.com", avatar_url: "https://i.ibb.co/kXC4Z2R/65136031766-8-BA4-B1-A2-1-C09-492-D-945-B-736-F9-AC31200.jpg", password: "12345")
-# User.create(username: "Ziggy", email: "ziggy@gmail.com", avatar_url: "https://i.ibb.co/L5HMdD1/64443805822-54-BF478-A-80-D0-451-A-949-E-56499350-AD4-D.jpg", password: "Ziggy")
-# User.create(username: "Shawn", email: "shawn@gmail.com", avatar_url: "https://i.ibb.co/kmGX9wn/IMG-3834.jpg", password: "Shawn")
-# User.create(username: "Amanda", email: "amanda@gmail.com", avatar_url: "https://i.ibb.co/8jgy38r/IMG-5647.jpg", password: "Amanda")
-# User.create(username: "Forrest", email: "forrest@gmail.com", avatar_url: "https://i.ibb.co/H7CLc2V/IMG-5617.jpg", password: "Bug")
+# User.create(username: "Sav", email: "savannaac@gmail.com", avatar_url: "https://i.ibb.co/mhGXYYP/67193423365-56-D81-C48-62-C0-434-C-B8-BC-85-B8-E3-C33-C89.jpg", password: "12345")
+# User.create(username: "Ziggy", email: "ziggy@gmail.com", avatar_url: "https://i.ibb.co/SBxFVLd/IMG-9097.jpg", password: "Ziggy")
 
 # Conversation.create(name: "<3")
 # Conversation.create
